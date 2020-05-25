46ab26fbf92cc4a212da1d88f4baf9761173222c
p_landyhli <p_landyhli@tencent.com>
2020-05-25 17:33:33 +0800

    init

---
+++ src/main/java/com/lyh/test/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.lyh.test.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/{name}")
+    public String name(@PathVariable("name") String name){
+        return "Hello, " + name;
+    }
+}
